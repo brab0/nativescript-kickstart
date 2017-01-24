3ad645be77f0205137bcc347fd8ccaa6477516da
Rodrigo Brabo <brabo.rodrigo@gmail.com>
2017-01-24 02:38:31 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ else
 fi
 
 if [ $has_npm == false ]; then
-  read -p "This project requires NPM to work properly. Shall we install it for you (y/n)? " answer
+  read -p "This project requires NPM to work properly. Shall we install it for you \(y/n\)? " answer
   case ${answer:0:1} in
       y|Y )
           echo 'Installing NPM...'
@@ -34,7 +34,7 @@ if [ $has_npm == false ]; then
 fi
 
 if [ $has_node == false ]; then
-  read -p "This project requires NodeJs to work properly. Shall we install it for you (y/n)? " answer
+  read -p "This project requires NodeJs to work properly. Shall we install it for you \(y/n\)? " answer
   case ${answer:0:1} in
       y|Y )
           echo 'Installing NodeJs...'
@@ -52,7 +52,7 @@ if [ $has_node == false ]; then
 fi
 
 if [ $has_tns == false ]; then
-  read -p "Do you wanna install NativeScript globally (y/n)? " answer
+  read -p "Do you wanna install NativeScript globally \(y/n\)? " answer
   case ${answer:0:1} in
       y|Y )
         echo 'Installing NativeScript...'
@@ -67,7 +67,7 @@ if [ $has_tns == false ]; then
 fi
 
 if [ $has_git == false ]; then
-  read -p "Do you wanna install Git (y/n)? " answer
+  read -p "Do you wanna install Git \(y/n\)? " answer
   case ${answer:0:1} in
       y|Y )
         if [ "$os" == "linux" ]; then
@@ -75,7 +75,7 @@ if [ $has_git == false ]; then
           sudo apt-get install git-all
         elif [ "$os" == "ios" ]; then
           if [ $has_brew == false ]; then
-            read -p "You need to install Homebrew. Do you want we install it for you (y/n)? " answer
+            read -p "You need to install Homebrew. Do you want we install it for you \(y/n\)? " answer
             case ${answer:0:1} in
                 y|Y )
                 echo 'Installing Homebrew...'
@@ -101,50 +101,48 @@ if [ $has_git == false ]; then
   esac
 fi
 
-#if [ $has_project == false ]; then
-#  read -p "Do you wanna clone this NativeScript Kickstart project from Github's repository (y/n)? " answer
-#  case ${answer:0:1} in
-#      y|Y )
-#        read -p "Do you wanna change the directory's name ($repo)? (y/n)? " answer
-#        case ${answer:0:1} in
-#            y|Y )
-#              read -p "Type the new name: " repo
-#            ;;
-#            * )
-#              echo "No, the current name is fine!"
-#            ;;
-#        esac
+if [ $has_project == false ]; then
+  read -p "Do you wanna clone this NativeScript Kickstart project from Github's repository \(y/n\)? " answer
+  case ${answer:0:1} in
+      y|Y )
+        read -p "Do you wanna change the directory's name \($repo\)? \(y/n\)? " answer
+        case ${answer:0:1} in
+            y|Y )
+              read -p "Type the new name: " repo
+            ;;
+            * )
+              echo "No, the current name is fine!"
+            ;;
+        esac
+        git clone "https://github.com/brab0/nativescript-kickstart $repo"
+        cd $repo
+        clear
+        echo "...repository cloned!"
+      ;;
+      * )
+        echo "No, thanks!"
+      ;;
+  esac
+fi
 
-#        git clone "https://github.com/brab0/nativescript-kickstart $repo"
-#        cd $repo
-#        clear
-#        echo "...repository cloned!"
+read -p "Do you wanna link this project to another remote Git repository \(y/n\)? " answer
+  case ${answer:0:1} in
+    y|Y )
+        read -p "Type the repository's url: " input_variable
 
-#      ;;
-#      * )
-#        echo "No, thanks!"
-#      ;;
-#  esac
-#fi
+        sudo git remote rename origin upstream
+        sudo git remote add origin $input_variable
+        sudo git push origin master
 
-#read -p "Do you wanna link this project to another remote Git repository (y/n)? " answer
-#  case ${answer:0:1} in
-#    y|Y )
-        #read -p "Type the repository's url: " input_variable
+        clear
+        echo "...your project was successfully pushed!"
+    ;;
+    * )
+        echo "Not yet!"
+    ;;
+esac
 
-        #sudo git remote rename origin upstream
-        #sudo git remote add origin $input_variable
-        #sudo git push origin master
-
-        #clear
-#        echo "...your project was successfully pushed!"
-#    ;;
-#    * )
-#        echo "Not yet!"
-#    ;;
-#esac
-
-read -p "Do you wanna install the project's dependencies now (y/n)? " answer
+read -p "Do you wanna install the project's dependencies now \(y/n\)? " answer
 case ${answer:0:1} in
     y|Y )
         sudo npm install
@@ -156,7 +154,7 @@ case ${answer:0:1} in
     ;;
 esac
 
-read -p "Do you wanna add Android's platform to the project (y/n)? " answer
+read -p "Do you wanna add Android's platform to the project \(y/n\)? " answer
 case ${answer:0:1} in
     y|Y )
         sudo tns platform add android
@@ -169,7 +167,7 @@ case ${answer:0:1} in
 esac
 
 if [ "$os" == "ios" ]; then
-  read -p "Do you wanna add iOS's platform to the project(y/n)? " answer
+  read -p "Do you wanna add iOS's platform to the project \(y/n\)? " answer
   case ${answer:0:1} in
       y|Y )
         sudo tns platform add ios

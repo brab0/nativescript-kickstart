066770b5e93db92f375b83eb38ef632002729096
Rodrigo Brabo <brabo.rodrigo@gmail.com>
2017-01-24 18:34:16 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ else
     os="ios"
 fi
 
-#if [ $has_npm == false ]; then
+if [ $has_npm == false ]; then
     read -p "This project requires NPM to work properly. Shall we install it for you [y/n]? " answer < /dev/tty
     if [ "$answer" == [Yy]* ]; then
         echo "Installing NPM..."
@@ -26,23 +26,23 @@ fi
     else
         echo "No, I will do it for my self!"
     fi
-#fi
+fi
 
-#if [ $has_node == false ]; then
+if [ $has_node == false ]; then
     read -p "This project requires NodeJs to work properly. Shall we install it for you [y/n]? " answer < /dev/tty
     if [ "$answer" == [Yy]* ]; then
         echo "Installing NodeJs..."
-        npm cache clean -f
-        npm install -g n
-        n stable
+        sudo npm cache clean -f
+        sudo npm install -g n
+        sudo n stable
         clear
         echo "...NodeJs version $(node --version) installed!"
     else
         echo "You're on your own!"
     fi
-#fi
+fi
 
-#if [ $has_tns == false ]; then
+if [ $has_tns == false ]; then
     read -p "This project requires NativeScript to work properly. Do you wanna install it globally [y/n]? " answer < /dev/tty
     if [ "$answer" == [Yy]* ]; then
         echo "Installing NativeScript..."
@@ -52,9 +52,9 @@ fi
     else
         echo "hum..."
     fi
-#fi
+fi
 
-#if [ $has_git == false ]; then
+if [ $has_git == false ]; then
     read -p "Do you wanna install Git [y/n]? " answer < /dev/tty
     if [ "$answer" == [Yy]* ]; then
         if [ "$os" == "linux" ]; then
@@ -68,6 +68,8 @@ fi
                     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
                     echo "Installing GIT..."
                     sudo brew install git
+                else
+                    echo "All right!"              
                 fi
             else
                 echo "All right!"              
@@ -78,28 +80,32 @@ fi
     else
         echo "Whaaat???"
     fi
-#fi
+fi
 
-#if [[ $has_project == false ] && [ $has_git == true ]]; then
-    read -p "Do you wanna clone this NativeScript Kickstart project from Github repository [y/n]? " answer < /dev/tty
-    if [ "$answer" == [Yy]* ]; then
-        read -p "Do you wanna change the directorys name ($repo)? [y/n]? " answer < /dev/tty
+if [ $has_project == false ]; then
+    if [ $has_git == true ]; then
+        read -p "Do you wanna clone this NativeScript Kickstart project from Github repository [y/n]? " answer < /dev/tty
         if [ "$answer" == [Yy]* ]; then
-            read -p "Type the new name: " repo < /dev/tty
-        else
-            echo "No, the current name is fine!"
-        fi
-    
-        git clone "https://github.com/brab0/nativescript-kickstart $repo"
-        cd $repo
-        clear
-        echo "...repository cloned!"
-    else  
-        echo "=("
-    fi
-#fi
+            read -p "Do you wanna change the directorys name ($repo)? [y/n]? " answer < /dev/tty
+            if [ "$answer" == [Yy]* ]; then
+                read -p "Type the new name: " repo < /dev/tty
+            else
+                echo "No, the current name is fine!"
+            fi
 
-if [[ $has_git == true ] && [ $has_project == false ]]; then
+            git clone "https://github.com/brab0/nativescript-kickstart $repo"
+            cd $repo
+            clear
+            echo "...repository cloned!"
+        else  
+            echo "=("
+        fi
+    else
+        echo "...we could not clone the projet because you don't have Git installed...told you!"
+    fi
+fi
+
+if [ $has_project == true ]; then
     read -p "Do you wanna link this project to another remote Git repository [y/n]? " answer < /dev/tty
     if [ "$answer" == [Yy]* ]; then
         read -p "Type the repositorys url: " input_variable < /dev/tty
@@ -113,38 +119,45 @@ if [[ $has_git == true ] && [ $has_project == false ]]; then
     fi
 fi
 
-if [[ $has_npm == true ] && [ $has_project == true ]]; then
-    read -p "Do you wanna install the projects dependencies now [y/n]? " answer < /dev/tty
-    if [ "$answer" == [Yy]* ]; then
-        npm install
-        clear
-        echo "...repository cloned!"
+if [ $has_project == true ]; then
+    if [ $has_npm == true ]; then
+        read -p "Do you wanna install the projects dependencies now [y/n]? " answer < /dev/tty
+        if [ "$answer" == [Yy]* ]; then
+            npm install
+            clear
+            echo "...repository cloned!"
+        else
+            echo "Ill do it later!"
+        fi
     else
-        echo "Ill do it later!"
-        break
+        echo "We cannot install the project's depencies 'cause you don't have npm installed...I told you!"
     fi
 fi
 
-if [[ $has_tns == true ] && [ $has_project == true ]]; then
-    read -p "Do you wanna add Androids platform to the project [y/n]? " answer < /dev/tty
-    if [ "$answer" == [Yy]* ]; then
-        tns platform add android
-        clear
-        echo "...Androids platform added!"
-    else        
-        echo "Not really!"
-    fi
-fi
-
-if [[ "$os" == "ios" ] && [ $has_tns == true ] && [ $has_project == true ]]; then
-    read -p "Do you wanna add iOSs platform to the project [y/n]? " answer < /dev/tty
-    if [ "$answer" == [Yy]* ]; then
-        sudo tns platform add ios
-        clear
-        echo "iOSs platform added!"
+if [ $has_project == true ]; then
+    if [ $has_tns == true ]
+        read -p "Do you wanna add Androids platform to the project [y/n]? " answer < /dev/tty
+        if [ "$answer" == [Yy]* ]; then
+            tns platform add android
+            clear
+            echo "...Androids platform added!"
+        else        
+            echo "Not really!"
+        fi
+        
+        if [ "$os" == "ios" ]; then
+            read -p "Do you wanna add iOSs platform to the project [y/n]? " answer < /dev/tty
+            if [ "$answer" == [Yy]* ]; then
+                sudo tns platform add ios
+                clear
+                echo "iOSs platform added!"
+            else
+                echo "No, Thanks!"
+            fi 
+        fi
     else
-        echo "No, Thanks!"
-    fi 
+        echo "We cannot not install add mobile platforms 'cause you don't have tns installed...told you!"
+    fi       
 fi
 
 echo "Enjoy!"
